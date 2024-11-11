6c49b301357850047b7f0ca2b5b8bd889d2d70ec
Sreegul G <sreegulg@Sreeguls-MacBook-Air.local>
2024-11-11 22:27:33 +0530

    adding and declaring variables

---
+++ variables/script.js
@@ -0,0 +1,23 @@
+// declaration
+let x;
+
+// initialization
+x = 100;
+
+console.log(x);
+
+// declaration & initialization
+let y = 12;
+console.log(y);
+
+// declaration of multiple variables
+let a, b, c;
+a = 1;
+b = 2;
+c = 3;
+console.log(a, b, c);
+
+// declaration & initialization of multiple variables
+
+let t = 'abc', q = 23, r = 100;
+console.log(t, q, r);
