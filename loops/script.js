f212e40c7ed2d289897620d7e53c2f8b25e77fff
Sreegul G <sreegulg@Sreeguls-MacBook-Air.local>
2024-11-15 19:54:44 +0530

    loops in js

--- loops/script.js
+++ loops/script.js
@@ -54,51 +54,16 @@ while (i <= 10) {
 
 
 // do while loop and if else loop
-// console.log("do while loop and if else loop");
+console.log("do while loop and if else loop");
 
-// let randNum = Math.trunc(Math.random() * 10);
-
-// do {
-//   guessNum = prompt("Guess a number from 0 to 10");
-//   if (guessNum == randNum) {
-//     console.log("Correctly guessed", randNum);
-//   }
-//   else {
-//     console.log("Try again");
-//   }
-// }
-// while (randNum != guessNum);
-
-function sumOfPrimeNumbers(limit) {
-  let answer = 0; // Variable to store the sum of all prime numbers
-
-  // Iterate through all numbers from 0 to the given limit
-
-  // Inside the first loop create a variable to count the number of divisors of i
-
-  // Check how many numbers divide i without a remainder by using nested loop
-
-
-  // If count equals 2, then i is a prime number
-  isPrime = true;
-  for (let i = 2; i <= limit; i++) {
-    for (let j = 2; j < i; j++) {
-      if (i % j == 0) {
-        isPrime = false;
-        break;
-      }
-
-    }
-    if (isPrime) {
-      console.log(i);
-      answer += i;
-    }
-    isPrime = true;
+let randNum = Math.trunc(Math.random() * 10);
+do {
+  guessNum = prompt("Guess a number from 0 to 10");
+  if (guessNum == randNum) {
+    console.log("Correctly guessed", randNum);
   }
-
-  // Return the sum of all prime numbers between 0 and limit
-  return answer;
+  else {
+    console.log("Try again");
+  }
 }
-
-
-console.log(sumOfPrimeNumbers(50));
+while (randNum != guessNum);
